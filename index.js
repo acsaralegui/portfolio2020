c7ec7e602e12573b76ef9252133d20feafd23704
acsaralegui <aaron.saralegui@gmail.com>
2020-06-02 14:40:01 -0700

    change index.html

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('src', path.join(__dirname, 'src'))
-  .set('view engine', 'handlebars')
-  .get('/', (req, res) => res.render('index'))
+const app = express ();
+app.get('/', (req, res)=> { res.send("index.html") })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
