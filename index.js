1e66f1076e3fc57566835e7c177a9788a5bf90b8
acsaralegui <aaron.saralegui@gmail.com>
2020-06-02 14:44:19 -0700

    change files

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const app = express ();
-app.get('/', (req, res)=> { res.send("index.html") })
+app.get('/', (req, res)=> { res.send("index") })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
