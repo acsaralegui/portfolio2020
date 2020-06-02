1b503f285d851402ed4fd5197fdcd6282345ffaa
acsaralegui <aaron.saralegui@gmail.com>
2020-06-02 09:13:55 -0700

    change view engine

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('src', path.join(__dirname, 'src'))
-  .set('src engine', 'html')
+  .set('view engine', 'jade')
   .get('/', (req, res) => res.render('index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
