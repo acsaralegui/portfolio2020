e2838ee2041537081d509084b8e7739035929236
acsaralegui <aaron.saralegui@gmail.com>
2020-06-02 09:00:07 -0700

    change index.jspart 3

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('src/index'))
+  .set('src', path.join(__dirname, 'src'))
+  .set('view engine', 'js')
+  .get('/', (req, res) => res.render('index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
