3dd042dc0b33edcc4090ed74be77a0ed124a8cc2
acsaralegui <aaron.saralegui@gmail.com>
2020-06-02 15:25:46 -0700

    going to the original commit

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-const app = express ();
-app.get('/', (req, res)=> { res.send("index") })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
